8b8855d135c8ea1dfca255b90ad2783e183ef1e8
Klementyna <klementyna.lewinska@gmail.com>
2024-06-18 15:04:46 +0200

    with open()
    wiele błędów, ale zaczęło dodawać dane do pliku

--- skrypt.py
+++ skrypt.py
@@ -17,7 +17,7 @@ def files_list(files):
 	return files_list
 list_of_files = files_list(folder)
 
-verse1=['mean_int','mean_int_bg' ,'int_intensity','area']
+verse1=['filename','mean_int','mean_int_bg','int_intensity','area']
 with open(r'C:\Users\Zuzan\OneDrive\Pulpit\Klementyna\GitHub\stackoverflow_imageJ\data.csv','w') as f:
 	writer=csv.writer(f, delimiter='\t')
 	writer.writerow(verse1)
@@ -38,18 +38,19 @@ for n in list_of_files[:4]:
 	run("Make Inverse");
 	run("Measure");
 	rt = RT.getResultsTable()
-	mean_intensity = rt.getValue("Mean",0) 
-	mean_intensity_bg = rt.getValue("Mean",1) 
-	int_intensity = rt.getValue("IntDen",0)
+	mean_intensity=rt.getValue("Mean",0) 
+	mean_intensity_bg=rt.getValue("Mean",1) 
+	int_intensity=rt.getValue("IntDen",0)
 	Area = rt.getValue("Area",0)
 	run("Close")
 	run("Close")
-	measurements=[mean_intensity,mean_intensity_bg ,int_intensity,Area ]
-	''''with open(r'C:\Users\Zuzan\OneDrive\Pulpit\Klementyna\GitHub\stackoverflow_imageJ\data.csv','a') as f:
+	measurements=[mean_intensity,mean_intensity_bg,int_intensity,Area]
+	print(measurements)
+	with open(r'C:\Users\Zuzan\OneDrive\Pulpit\Klementyna\GitHub\stackoverflow_imageJ\data.csv','a') as f:
 		writer=csv.writer(f, delimiter='\t')
-		f.seek(0, 2)  # Przesuń kursor na koniec pliku
-        if f.tell() > 0
-        	f.write('\n')
+	#	f.seek(0, 2)  # Przesuń kursor na koniec pliku
+		if f.tell() > 0:
+				f.write('\n')
 		writer.writerow(measurements)
 	
 	#chcę to wszystko zapisać w pliku i najlepiej jeszcze nazwę pliku .csv .tsv (taby - /T?) i jeszcze zadbac zeby bylo wszystko od nowej linii (otworzyc plik, zapisac, zamknac plik) - zadanie domowe:)
