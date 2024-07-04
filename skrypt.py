268a75125f15fdd57ab902a66861c22200dce40d
Klemucha <klementyna.lewinska@gmail.com>
2024-07-04 12:27:59 +0200

    Channel i mtype

--- skrypt.py
+++ skrypt.py
@@ -4,14 +4,14 @@ from ij.IJ import run, setThreshold, setAutoThreshold
 from ij.measure import ResultsTable as RT
 import csv
 
-
-
-folder = r"C:\Users\Klementyna\Desktop\STUDIA\magisterka_neuro\Barwienie_PoliADPr\07.02_PoliADP\P4sca7"
+channel = "Ch2"
+mtype = "WT3"
+folder = r"C:\Users\Klementyna\Desktop\STUDIA\magisterka_neuro\Barwienie_PoliADPr\IS_pADPR_RanBP2_100624\\" + mtype + r"\P4"
 def files_list(directory):
 	files_list = []
 	for root, dirs, files in os.walk(directory):
 		for file in files:
-			if "Ch11" in file:      #warunek na ch11 w nazwie
+			if channel in file:      #warunek na ch2 w nazwie
 				file_path = os.path.join(root,file)   #full path of the file
 				files_list.append((file_path,file))   #append a tuple of file path and file name to the list
 	return files_list
@@ -19,16 +19,11 @@ list_of_files = files_list(folder)
 print(len(list_of_files))
 
 verse1=['filename','mean_int','mean_int_bg','int_intensity','area']
-with open(r'C:\Users\Klementyna\Desktop\STUDIA\magisterka_neuro\data.csv','w') as f:
+with open(r'C:\Users\Klementyna\Desktop\STUDIA\magisterka_neuro\\' + mtype + channel + r'.csv','w') as f:
 	writer=csv.writer(f, delimiter='\t')
 	writer.writerow(verse1)
-# Save list_of_files to a new CSV file
-with open(r'C:\Users\Klementyna\Desktop\STUDIA\magisterka_neuro\list_of_files.csv', 'w') as f:
-    writer = csv.writer(f, delimiter='\t', lineterminator='\n')
-    writer.writerow(['file_path', 'file_name'])  # write the header
-    writer.writerows(list_of_files)  # write the data
-    
 
+   
 for n in list_of_files:
 	print(n[1])						 #n[0] czyli pierwszy element w n (u nas ścieżka do pliku)
 	im = IJ.open(n[0]) 				#otwieramy plik
@@ -52,7 +47,7 @@ for n in list_of_files:
 	Area = rt.getValue("Area",0)
 	measurements=[n[1],mean_intensity,mean_intensity_bg,int_intensity,Area]
 	print(measurements)
-	with open(r'C:\Users\Klementyna\Desktop\STUDIA\magisterka_neuro\data.csv','a') as f:
+	with open(r'C:\Users\Klementyna\Desktop\STUDIA\magisterka_neuro\\' + mtype + channel + r'.csv','a') as f:
 		writer=csv.writer(f, delimiter='\t',lineterminator='\n')
 		writer.writerow(measurements)
 	
